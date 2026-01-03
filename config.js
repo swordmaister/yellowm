b8436d04d4a46942dae137febfbf2479ca1d59ad
swordmaister <syulingdingzing@gmail.com>
2026-01-03 10:08:50 +0900

    Add configuration file for game settings

---
+++ config.js
@@ -0,0 +1,16 @@
+export const CFG = {
+  colors: {
+    sky: 0x87CEEB,
+    ground: 0xC2B280,
+    player: 0x00ff00,
+    enemy: 0xff4444,
+    kekkai: 0xffff00
+  },
+  player: {
+    speed: 10.0,
+    maxHp: 100
+  },
+  world: {
+    gravity: -30
+  }
+};
